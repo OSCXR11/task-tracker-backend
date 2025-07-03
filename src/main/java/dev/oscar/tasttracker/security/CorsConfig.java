6a8fc7d7d2d997dd5b93abb75ed47047bb15531c
OSCXR11 <oscaravendano@yahoo.com>
2025-07-02 22:44:13 -0700

    update url

--- src/main/java/dev/oscar/tasttracker/security/CorsConfig.java
+++ src/main/java/dev/oscar/tasttracker/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://task-tracker-oscar.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
