8e2bb41f4301d96773e5ab315e75222de425b99b
OSCXR11 <oscaravendano@yahoo.com>
2025-07-02 21:48:43 -0700

    Initial commit

--- src/main/java/dev/oscar/tasttracker/TasktrackerApplication.java
+++ src/main/java/dev/oscar/tasttracker/TasktrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TasttrackerApplication {
+public class TasktrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TasttrackerApplication.class, args);
+		SpringApplication.run(TasktrackerApplication.class, args);
 	}
 
 }
